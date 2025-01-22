4948ec3b994ce36b6d8b8a6d8db06079a95257e9
ANSIMA Elvis <ansimapersic@gmail.com>
2025-01-22 14:53:34 +0200

    basic config for nuxt seo and dynamic sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,16 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui'],
+  modules: ['@nuxt/ui', '@nuxtjs/seo'],
   css: ['@fortawesome/fontawesome-free/css/all.css'],
   colorMode: {
     preference: 'dark'
-  }
+  },
+  site: {
+    url: 'https://ansima.dev',
+    name: 'Ansima Portfolio Site',
+  },
+  sitemap: {
+    sources: ['/api/__sitemap__/urls'],
+  },
 })
